ff2c64c6302b39095c7a46972f78e8b3218cbb33
Viktor Stefanov <victorystefanov@gmail.com>
2024-10-06 01:19:02 +0300

    add state for story text and re-render typewritter when change

--- turning-titans/src/pages/Game/Game.jsx
+++ turning-titans/src/pages/Game/Game.jsx
@@ -5,12 +5,10 @@ import Box from '@mui/material/Box';
 import StoryText from '../../components/StoryText/StoryText';
 import Options from '../../components/Options/Options';
 
-const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus';
-
 // const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas!';
 
 const Game = () => {
-  const [isSelected, setIsSelected] = useState(false);
+  const [storyText, setStoryText] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus');
   const [animationKey, setAnimationKey] = useState(0);
   const [ onFullTextDisplayed, setOnFullTextDisplayed ] = useState(false);
 
@@ -18,11 +16,10 @@ const Game = () => {
   ]);
 
   const onClick = () => {
-    if(!isSelected) {
-      setIsSelected(true);
-    }
+    setStoryText('Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a earum impedit ducimus, fugiat minus eum tempora nihil quo debitis incidunt aut enim qui, vitae cupiditate inventore harum. Enim, quas!');
     setOptions(['1', '2', '3', '4']);
     setAnimationKey(prev => prev + 1);
+    setOnFullTextDisplayed(false);
   };
 
   const onDone = () => {
@@ -31,7 +28,7 @@ const Game = () => {
 
   return (
     <Box sx={{ padding: '10rem 40rem', textAlign: 'justify', display: 'flex', flexDirection: 'column', gap: '4rem', justifyContent: 'center', alignItems: 'center' }}>
-      {!isSelected && <StoryText text={text} onDone={onDone}/>}
+      <StoryText text={storyText} onDone={onDone}/>
       {onFullTextDisplayed && <Options onClick={onClick} options={options} key={animationKey}/>}
     </Box>
   )
