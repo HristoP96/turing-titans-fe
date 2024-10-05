016cf084662d1f798cf3e075d2a0e912ee3bc85e
Viktor Stefanov <victorystefanov@gmail.com>
2024-10-06 00:05:08 +0300

    change typewritting library, removing unused deps

--- turning-titans/src/components/Options/Options.jsx
+++ turning-titans/src/components/Options/Options.jsx
@@ -3,7 +3,6 @@ import Option from '../Option/Option'
 import './Options.css'
 
 const Options = ( { options, onClick }) => {
-    const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ad modi ratione nesciunt sequi expedita voluptatum officiis voluptates. Exercitationem quas aperiam eligendi, voluptates natus quam delectus pariatur vero labore odit."
   return (
     <div className='options'>
       {options.map((o, index) => (
